e1499698d15254d187309a86887b4f1d24af3527
joyislam1954 <joyislam1954@gmail.com>
2021-05-10 21:54:38 +0600

    create models name PersonalInformation and Migrate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class PersonalInformation(models.Model):
+    name = models.CharField(
+        max_length=254,
+        verbose_name='Name'
+    )
+    first_name = models.CharField(
+        max_length=100,
+        verbose_name='First Name'
+    )
+    last_name = models.CharField(
+        max_length=100,
+        verbose_name='Last Name'
+    )
+    email = models.EmailField(
+        verbose_name='Email'
+    )
+    country = models.CharField(
+        max_length=50,
+        verbose_name='Country'
+    )
+
+    def __str__(self):
+        return self.name
